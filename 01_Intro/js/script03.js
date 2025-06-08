b3aec730c9f3d07bc19b4518cb784a2d8ff6ccba
douaB-167 <doua.boufqir@edu.immaginazioneelavoro.it>
2025-06-08 17:23:26 +0200

    Add files via upload

---
+++ 01_Intro/js/script03.js
@@ -0,0 +1,36 @@
+let listaSpesa = document.getElementById("listaSpesa")
+
+let prodotti = ["carta", "passata di pomodoro", "philadelphia", "mozzarella", "spaghetti", "olio d'oliva"];
+let prezzi = [2.39, 0.99, 3.50, 1.99, 1.20, 3.99];
+let quants = [1, 3, 1, 1, 2, 1];
+
+let totaleGenerale = 0;
+let totaleElement = document.getElementById("totale");
+
+for(let i=0; i < prodotti.length; i++){
+    let tr = document.createElement("tr");
+
+    let tdNome = document.createElement("td");
+    tdNome.textContent = prodotti[i].toUpperCase().charAt(0) + prodotti[i].slice(1);
+    //tdNome.textContent = prodotti[i].charAt(0).toUpperCase() + prodotti[i].slice(1);
+
+    let tdPrezzo = document.createElement("td");
+    tdPrezzo.textContent = prezzi[i] + " €";
+    
+    let tdQuantita = document.createElement("td");
+    tdQuantita.textContent = quants[i];
+
+    let subTotale = prezzi[i] * quants[i];
+    let tdSubTotale = document.createElement("td");
+    tdSubTotale.textContent = subTotale.toFixed(2);
+
+    tr.appendChild(tdNome);
+    tr.appendChild(tdPrezzo);
+    tr.appendChild(tdQuantita);
+    tr.appendChild(tdSubTotale);
+
+    listaSpesa.appendChild(tr);
+    totaleGenerale += subTotale;
+}
+
+document.getElementById("totale").innerHTML = "Totale: " + totaleGenerale.toFixed(2) + " €";
